476c0d972d4bcf02cc0c90c48d779ac3193fd56a
Maggie <ml0729@students.cs.ubc.ca>
2021-11-08 20:25:51 -0800

    Phase3 Visual Components (gif display when application runs and close), method/class specification added

--- src/main/ui/VisionComponentsManager.java
+++ src/main/ui/VisionComponentsManager.java
@@ -10,6 +10,7 @@ public class VisionComponentsManager extends JFrame {
     private ImageIcon goodbyeImage;
     private JLabel goodbyeLabel;
 
+    //EFFECTS: set fields to correct image files and attach JLabel to ImageIcon (load images)
     public VisionComponentsManager() {
         String sep = System.getProperty("file.separator");
         welcomeImage = new ImageIcon(System.getProperty("user.dir") + sep + "gif"
@@ -21,12 +22,16 @@ public class VisionComponentsManager extends JFrame {
         goodbyeLabel = new JLabel(goodbyeImage);
     }
 
+    //MODIFIES: this
+    //EFFECTS: Layouts the welcomeImage and set pop-up window's title
     public void displayWelcomeImage() {
         setLayout(new FlowLayout());
         add(welcomeLabel);
         setFrame("Welcome Back!!");
     }
 
+    //MODIFIES: this
+    //EFFECTS: First remove the welcomeImage then layouts the goodbyeImage and set pop-up window's title
     public void displayGoodbyeImage() {
         setLayout(new FlowLayout());
         remove(welcomeLabel);
@@ -34,6 +39,8 @@ public class VisionComponentsManager extends JFrame {
         setFrame("Bye bye!!");
     }
 
+    //MODIFIES: this
+    //EFFECTS: set up and display the window
     private void setFrame(String title) {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
